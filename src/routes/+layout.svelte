e53f3f3d8f40f1f1818a71282c535a43404c67fc
Eamonn de Leastar <edeleastar@wit.ie>
2024-09-03 15:34:02 +0100

    first skeleton component

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,25 @@
-<script>
+<script lang="ts">
   import "../app.css";
+  import { AppBar } from "@skeletonlabs/skeleton-svelte";
+  import ArrowLeft from "lucide-svelte/icons/arrow-left";
+  import Paperclip from "lucide-svelte/icons/paperclip";
+  import Calendar from "lucide-svelte/icons/calendar";
+  import CircleUser from "lucide-svelte/icons/circle-user";
+  let { children } = $props();
 </script>
 
-<div class="grid h-screen grid-rows-[auto_1fr_auto]">
-  <!-- Header -->
-  <header class="bg-red-500 p-4">(header)</header>
-  <!-- Main -->
-  <slot></slot>
-  <!-- Footer -->
-  <footer class="bg-blue-500 p-4">(footer)</footer>
-</div>
+<AppBar background="bg-surface-100-800-token" classes="h-20 justify-center border-b-[1px] border-surface-200 dark:border-surface-700">
+  {#snippet lead()}
+    <ArrowLeft size={24} />
+  {/snippet}
+  {#snippet trail()}
+    <Paperclip size={20} />
+    <Calendar size={20} />
+    <CircleUser size={20} />
+  {/snippet}
+  {#snippet headline()}
+    <h2 class="h5">Oileain: Islands of Ireland</h2>
+  {/snippet}
+</AppBar>
+
+{@render children()}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,19 @@
-<main class="space-y-4 bg-green-500 p-4">
-  <p class="h-[512px] bg-purple-500 p-4">Paragraph 1</p>
-  <p class="h-[512px] bg-purple-500 p-4">Paragraph 2</p>
-  <p class="h-[512px] bg-purple-500 p-4">Paragraph 3</p>
-</main>
+<div class="flex min-h-full flex-col">
+  <!-- 3 column wrapper -->
+  <div class="mx-auto w-full max-w-7xl grow lg:flex xl:px-2">
+    <!-- Left sidebar & main wrapper -->
+    <div class="flex-1 xl:flex">
+      <div class="border-b border-gray-200 px-4 py-6 sm:px-6 lg:pl-8 xl:w-64 xl:shrink-0 xl:border-b-0 xl:border-r xl:pl-6">
+        <p class="h-[512px] bg-purple-500 p-4">Paragraph 3</p>
+      </div>
+
+      <div class="px-4 py-6 sm:px-6 lg:pl-8 xl:flex-1 xl:pl-6">
+        <p class="h-[512px] bg-purple-500 p-4">Paragraph 3</p>
+      </div>
+    </div>
+
+    <div class="shrink-0 border-t border-gray-200 px-4 py-6 sm:px-6 lg:w-96 lg:border-l lg:border-t-0 lg:pr-8 xl:pr-6">
+      <p class="h-[512px] bg-purple-500 p-4">Paragraph 3</p>
+    </div>
+  </div>
+</div>
